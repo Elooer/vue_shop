7251d5c7d0bdbce55b2aba84c34a743db79838f7
云冥 <8617882+wenzishengQD@user.noreply.gitee.com>
2021-09-16 21:16:47 +0800

    完成了报表功能的开发

---
+++ src/components/report/Report.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <div id="main" style="width: 600px; height: 400px"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+// 导入 ecahrts
+import echarts from "echarts";
+export default {
+  data() {},
+  created() {},
+  //   此时页面上的元素已经被渲染完毕了
+  mounted() {
+    var myChart = echarts.init(document.getElementById("main"));
+
+    // 准备数据的配置项
+    var option = {
+      title: {
+        text: "ECharts 入门示例",
+      },
+      tooltip: {},
+      legend: {
+        data: ["销量"],
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪绒衫", "裤子", "高跟鞋", "袜子"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "销量",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20],
+        },
+      ],
+    };
+
+    // 展示数据
+    myChart.setOption(option);
+  },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+</style>
